d9ec263c0a510c60a7f4ef38e242ca2726312e46
Charlie Jay <charliejbjay@gmail.com>
2024-08-22 09:40:13 +1000

    All basic functionality implemented

--- data/timeline.js
+++ data/timeline.js
@@ -3,7 +3,7 @@ export default [
         year: 'Today',
         title: 'Unemployed',
         duration: '3 months',
-        details: '',
+        details: 'My professional life before studying at General Assembly spanned two very different industries. After finishing high school in 2012 I worked as a Sail-Making Apprentice before completing my Certificate 3 in Textile Fabrication at TAFE NSW to qualify as a Sail-Making Tradesman. I completed my Under Graduate degree in Social Sciences whilst working as a Sail-Making Tradesman, before moving on to begin work at the NSW Teachers Federation branch of the Australian Education Union. The majority of my time at NSWTF was spent working as a Professional Support Advisor.',
     },
     {
         year: 'Today',
@@ -15,7 +15,7 @@ export default [
         year: 'Today',
         title: 'Unemployed',
         duration: '3 months',
-        details: '',
+        details: 'Outside of my professional life I enjoy CrossFit, cooking, reading, watching Formula 1, and hanging out with my partner and cat.',
     },
     {
         year: 'Today',
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import timeline from '../../data/timeline';
+import timeline from "../../data/timeline";
 import TimelineItem from "./TimelineItem";
 import Title from "./title";
 
@@ -8,7 +8,7 @@ const Timeline = () => {
         <div className="flex flex-col md:flex-row justify-center my-20">
             <div className="w-full md:w-7/12">
                 <Title>Timeline</Title>
-                {timeline.map(item => (
+                {timeline.map((item) => (
                     <TimelineItem
                         year={item.year}
                         title={item.title}
