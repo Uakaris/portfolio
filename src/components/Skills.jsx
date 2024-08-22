26859bda5afade7d7ee08e34dcb3eb7da5737afe
Charlie Jay <charliejbjay@gmail.com>
2024-08-22 12:49:07 +1000

    Implemented centered title for technical skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import skills from "../../data/skills";
 import SkillsItems from "./SkillsItems";
+import Title from "./title";
 
 const Skills = () => {
     return (
-        <div className="flex flex-col md:flex-row items-center justify-center pb-12">
-            <div className="grid grid-cols-5 md:grid-cols-10 lg:grid-cols-10 gap-4">
-                {skills.map((item) => (
-                    <SkillsItems
-                        icon={item.icon}
-                    />
-                ))}
+        <div>
+            <p className="flex justify-center pb-4">
+                <Title>Technical Skills</Title>
+            </p>
+            <div className="flex flex-col md:flex-row items-center justify-center pb-12">
+                <div className="grid grid-cols-5 md:grid-cols-10 lg:grid-cols-10 gap-4">
+                    {skills.map((item) => (
+                        <SkillsItems
+                            icon={item.icon}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
