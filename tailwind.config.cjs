034cb7ebe66fdf7b362280df1c3f959d824f8e0f
Charlie Jay <charliejbjay@gmail.com>
2024-08-24 10:53:32 +1000

    changed font to poppins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                inter: ["inter", "serif"],
+                inter: ["poppins", "serif"],
             },
         },
     },
