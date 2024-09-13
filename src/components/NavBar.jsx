9a38262f9ac3d8a795467e0a0b166958c667edd0
Charlie Jay <charliejbjay@gmail.com>
2024-09-13 11:24:32 +1000

    Added smoother animations to intro icons, navbar, and github icons in projects section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
             <div className="relative max-w-screen-lg mx-auto bg-white bg-opacity-10 group-hover:opacity-100 transition-opacity duration-300 rounded-full shadow-lg">
                 <nav className="flex justify-between items-center font-bold p-4">
                     <ul className="flex space-x-4 w-full dark:text-white">
-                        <li className="flex-1 text-center">
+                        <li className="flex-1 text-center transition-transform duration-300 transform hover:scale-110">
                             <Link
                                 to="portfolio/"
                                 className="hover:text-blue-400"
@@ -15,7 +15,7 @@ const NavBar = () => {
                                 About
                             </Link>
                         </li>
-                        <li className="flex-1 text-center">
+                        <li className="flex-1 text-center transition-transform duration-300 transform hover:scale-110">
                             <Link
                                 to="portfolio/projects"
                                 className="hover:text-blue-400"
@@ -23,7 +23,7 @@ const NavBar = () => {
                                 Projects
                             </Link>
                         </li>
-                        <li className="flex-1 text-center">
+                        <li className="flex-1 text-center transition-transform duration-300 transform hover:scale-110">
                             <Link
                                 to="portfolio/skills"
                                 className="hover:text-blue-400"
@@ -34,7 +34,7 @@ const NavBar = () => {
                         {/* <li className="flex-1 text-center">
                             <Link to="portfolio/currentProjects" className="text-gray-900 hover:text-gray-600">Current Projects</Link>
                         </li> */}
-                        <li className="flex-1 text-center">
+                        <li className="flex-1 text-center transition-transform duration-300 transform hover:scale-110">
                             <Link
                                 to="portfolio/career"
                                 className="hover:text-blue-400"
